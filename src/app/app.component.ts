cdb8ced97419e5b86c89e681d79edf6605ea7b8c
Bugay Sarikaya <bgysrky@gmail.com>
2023-11-24 22:00:49 +0300

    birinci gun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'gib';
+  makeRedRef: string = 'makeRed';
+  makeRedStyle: string = 'red';
+  imageRef: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  apiRef: string = 'https://angular.io';
+
+  //xss sanitize
+  script: string = '<script>alert("hello")</script>';
+  div: string = '<div>Hello world</div>'
+  safeHtml!: SafeHtml;
+  message: string = 'Hello World!';
+  isButtonDisabled: boolean = false;
+  dynamicValue: string = 'Initial Value';
+  value: string = '2way data binding';
+  email: string = '';
+
+  constructor(private sanitizier: DomSanitizer) {
+
+  }
+
+  ngOnInit() {
+    this.safeHtml = this.sanitizier.bypassSecurityTrustHtml(this.div);
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  updateDynamicValue(event: any) {
+    this.dynamicValue = event?.target.value;
+  }
+
+  activateHandler(val: any) {
+    console.log(val);
+  }
+
+  emailOnChange(value: string) {
+    console.log('Email: ', value);
+  }
+
+  emailChanged(val: any) {
+    console.log(val);
+  }  
+
 }
